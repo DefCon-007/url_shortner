59ef2079f8afdb81d32f1fc0986114ae9f6bbfd0
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2020-02-09 20:53:09 +0530

    Add tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from api.helper import add_new_url
+from api.models import ShortUrls
 
 # Create your tests here.
+class ShortURLTest(TestCase):
+    def test_create_link(self):
+        # Test creation without short name
+        new_short = add_new_url("https://www.google.com")
+        self.assertEqual(ShortUrls.objects.filter(short_name=new_short).count(), 1)
+        # Test with custom short name
+        add_new_url("https://www.google.com", "abc")
+        self.assertEqual(ShortUrls.objects.filter(short_name="abc").count(), 1)
